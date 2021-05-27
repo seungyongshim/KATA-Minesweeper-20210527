fa69b2333166b94a23c854a4a6c3e1666eaaf1b4
SeungYongShim <syshim@me.com>
2021-05-27 15:02:11 +0900

    Constucture 구현

--- src/Minesweeper/MineField.cs
+++ src/Minesweeper/MineField.cs
@@ -8,15 +8,25 @@ namespace Minesweeper
 {
     public class MineField
     {
-        private int v1;
-        private int v2;
-
-        public MineField(int v1, int v2)
+        public MineField(int width, int height)
         {
-            this.v1 = v1;
-            this.v2 = v2;
+            Width = width;
+            Height = height;
+
+            Cells = new Cell[Width * Height];
+
+            for (int j = 0; j < Height; j++)
+            {
+                for (int i = 0; i < Width; i++)
+                {
+                    Cells[i + j * Width] = new Cell();
+                }
+            }
+
         }
 
         public IList<Cell> Cells { get; set; }
+        public int Width { get; }
+        public int Height { get; }
     }
 }
